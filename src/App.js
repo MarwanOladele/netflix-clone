b8e152441f828a76d9090e48022d4dfa8e755ef8
MarwanOladele <oladelemarwan@gmail.com>
2023-04-04 17:34:49 +0100

    new commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import HomeScreen from "./components/homescreen/HomeScreen";
 
 function App() {
   return (
     <div className="App">
-     <h1>neflix</h1>
+      <HomeScreen />
     </div>
   );
 }
---
+++ src/components/homescreen/HomeScreen.jsx
@@ -0,0 +1,9 @@
+import './HomeScreen.css'
+
+const HomeScreen = () => {
+  return (
+    <div>HomeScreen</div>
+  )
+}
+
+export default HomeScreen
