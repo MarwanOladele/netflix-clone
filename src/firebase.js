889586314d186211360c0a7f17ca3a4951533ce7
MarwanOladele <oladelemarwan@gmail.com>
2023-04-04 17:31:03 +0100

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBpzlDErtMgjiTg2HZRCnM0Fgd60eqP0NM",
+    authDomain: "netflix-clone-dff27.firebaseapp.com",
+    projectId: "netflix-clone-dff27",
+    storageBucket: "netflix-clone-dff27.appspot.com",
+    messagingSenderId: "716207863869",
+    appId: "1:716207863869:web:2230e1f351d3ce46429254"
+  };
