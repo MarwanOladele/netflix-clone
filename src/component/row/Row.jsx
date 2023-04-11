601ff6e04e1877ccbdd3e138c22e1ef817a37083
MarwanOladele <oladelemarwan@gmail.com>
2023-04-11 12:28:05 +0100

    new commit

--- src/component/row/Row.jsx
+++ src/component/row/Row.jsx
@@ -1,6 +1,7 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./Row.css";
 import axios from "../../api/axios";
+import { MdArrowBackIosNew, MdArrowForwardIos } from "react-icons/md";
 
 const Row = ({ title, fetchUrl, isLargeRow = false }) => {
   const [movies, setMovies] = useState([]);
@@ -15,13 +16,26 @@ const Row = ({ title, fetchUrl, isLargeRow = false }) => {
   }, [fetchUrl]);
 
   console.log(movies);
+  const scrollRef = useRef();
+
+  const scroll = (direction) => {
+    const { current } = scrollRef;
+
+    console.log(current);
+
+    if (direction === "left") {
+      current.scrollLeft -= 500;
+    } else {
+      current.scrollLeft += 500;
+    }
+  };
 
   const baseurl = "https://image.tmdb.org/t/p/original/";
 
   return (
     <div className="row">
       <h2>{title}</h2>
-      <div className="row__posterClass">
+      <div className="row__posterClass" ref={scrollRef}>
         {movies.map((movie) => (
           <img
             key={movie.id}
@@ -33,6 +47,14 @@ const Row = ({ title, fetchUrl, isLargeRow = false }) => {
           />
         ))}
       </div>
+      <MdArrowBackIosNew
+        className={`galley_display_arrowLeft ${isLargeRow && "display_Large"}`}
+        onClick={() => scroll("left")}
+      />
+      <MdArrowForwardIos
+        className={`galley_display_arrowRight ${isLargeRow && "display_Large"}`}
+        onClick={() => scroll("right")}
+      />
     </div>
   );
 };
