1c453862cd0882abe6697dfb9d97fe47341ae781
MarwanOladele <oladelemarwan@gmail.com>
2023-04-18 18:43:44 +0100

    redux implemented

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import "./App.css";
 import HomeScreen from "./pages/homescreen/HomeScreen";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/login/Login";
-import { auth } from "./firebase";
 import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./firebase";
 
 function App() {
   const user = false;
@@ -16,8 +16,12 @@ function App() {
 
   useEffect(() => {
     const unsub = onAuthStateChanged(auth, (userAuth) => {
-      if (userAuth) {
-        console.log(userAuth);
+      if (user) {
+        // User is signed in, see docs for a list of available properties
+        // https://firebase.google.com/docs/reference/js/firebase.User
+        const uid = userAuth.uid;
+        console.log(uid);
+        // ...
       } else {
         // User is signed out
         // ...
